e493aa67a23ded454ba73727a03e6c3b7f6e3b2a
codewithbandaeali <ibrahimkamran490@gmail.com>
2024-10-05 22:48:19 +0500

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-import os 
+import os
+#import magic_bin as magic
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import DeepLake
-from langchain.chat_models import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain.chains import RetrievalQA
-from dotenv import load_dotenv 
-import streamlit as st 
+from dotenv import load_dotenv
+import streamlit as st
 from streamlit_chat import message
 
+# Load environment variables
 load_dotenv()
 
-os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 os.environ['ACTIVELOOP_TOKEN'] = os.getenv('ACTIVELOOP_TOKEN')
-os.environ['DEEPLAKE_ACCOUNT_NAME']= os.getenv('DEEPLAKE_ACCOUNT_NAME')
+os.environ["GOOGLE_API_KEY"]='AIzaSyDTdH68aWCl5JWvPSF22QocrG_Cn7XrZM4'
+
 
 @st.cache_data
 def doc_preprocessing():
@@ -24,7 +27,7 @@ def doc_preprocessing():
     )
     docs = loader.load()
     text_splitter = CharacterTextSplitter(
-        chunk_size=1000, 
+        chunk_size=1000,
         chunk_overlap=0
     )
     docs_split = text_splitter.split_documents(docs)
@@ -32,15 +35,13 @@ def doc_preprocessing():
 
 @st.cache_resource
 def embeddings_store():
-    embeddings = OpenAIEmbeddings()
-    print(embeddings)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     texts = doc_preprocessing()
-    db = DeepLake.from_documents(texts, embeddings, dataset_path=f"hub://aianytime07/text_embedding")
-    print(db)
+    db = DeepLake.from_documents(texts, embeddings, dataset_path=f"hub://ibrahimkamran490/text_embedding")
     db = DeepLake(
-    dataset_path=f"hub://aianytime07/text_embedding",
-    read_only=True,
-    embedding_function=embeddings,
+        dataset_path=f"hub://ibrahimkamran490/text_embedding",
+        read_only=True,
+        embedding_function=embeddings,
     )
     return db
 
@@ -48,11 +49,11 @@ def embeddings_store():
 def search_db():
     db = embeddings_store()
     retriever = db.as_retriever()
-    retriever.search_kwargs['distance_metric'] = 'cos'
-    retriever.search_kwargs['fetch_k'] = 100
-    retriever.search_kwargs['maximal_marginal_relevance'] = True
-    retriever.search_kwargs['k'] = 10
-    model = ChatOpenAI(model='gpt-3.5-turbo')
+    #retriever.search_kwargs['distance_metric'] = 'cos'
+    #retriever.search_kwargs['fetch_k'] = 100
+    #retriever.search_kwargs['maximal_marginal_relevance'] = True
+    #retriever.search_kwargs['k'] = 10
+    model = ChatGroq(model="llama3-70b-8192")
     qa = RetrievalQA.from_llm(model, retriever=retriever, return_source_documents=True)
     return qa
 
@@ -62,7 +63,7 @@ qa = search_db()
 def display_conversation(history):
     for i in range(len(history["generated"])):
         message(history["past"][i], is_user=True, key=str(i) + "_user")
-        message(history["generated"][i],key=str(i))
+        message(history["generated"][i], key=str(i))
 
 def main():
     # Initialize Streamlit app with a title
@@ -76,11 +77,10 @@ def main():
         st.session_state["generated"] = ["I am ready to help you"]
     if "past" not in st.session_state:
         st.session_state["past"] = ["Hey there!"]
-        
+
     # Search the database for a response based on user input and update session state
     if user_input:
         output = qa({'query': user_input})
-        # print(output['source_documents'])
         st.session_state.past.append(user_input)
         response = str(output["result"])
         st.session_state.generated.append(response)
@@ -91,11 +91,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
